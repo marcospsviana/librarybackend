0ee8c555a8c3c19c62f797dfedd4b4dd1171c9f0
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-10-15 10:36:33 -0300

    add view book in library, add tests, facade_book

--- librarybackend/library/views.py
+++ librarybackend/library/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+
+def books():
+    books_list = Book.objects.order_by("title").all()
+    books = [
+        {
+            "id": 1,
+            "title": book.title,
+            "publisher_company": book.publish_company,
+            "photo": book.photo,
+            "authors": [author.name for author in book.author],
+        }
+    ]
--- librarybackend/settings.py
+++ librarybackend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'librarybackend.base',
+    'librarybackend.library'
 ]
 
 MIDDLEWARE = [
