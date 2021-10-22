d1ab617bc9e31070082acef44ad2c174698451cb
marcos p s viana <marcospaulo.silvaviana@gmail.com>
2021-10-22 11:05:41 -0300

    Developer (#7)
    * test delete book
    
    * add ci django
    
    * modificado model author para related_name, criado admin library, add put view

--- librarybackend/library/models.py
+++ librarybackend/library/models.py
@@ -7,6 +7,9 @@ class PublishCompany(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=200, null=False)
 
+    def __str__(self):
+        return f"{self.name}"
+
     class Meta:
         ordering = ["name"]
         verbose_name_plural = "names"
@@ -25,8 +28,10 @@ class Book(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
     title = models.CharField(max_length=200, null=False)
     photo = models.URLField(verbose_name="book_cover", max_length=500, null=True)
-    author = models.ManyToManyField(Author)
-    publish_company = models.ManyToManyField(PublishCompany)
+    author = models.ManyToManyField(Author, related_name="author")
+    publish_company = models.ManyToManyField(
+        PublishCompany, related_name="publish_company"
+    )
 
     class Meta:
         ordering = ["title"]
--- librarybackend/library/views.py
+++ librarybackend/library/views.py
@@ -1,6 +1,8 @@
-from .models import Book
+from .models import Book, Author, PublishCompany
+
 from django.http import HttpResponse
 from django.views import View
+import json
 
 
 class Books(View):
@@ -39,3 +41,30 @@ class BookDelete(View):
     def delete(self, request, id):
         Book.objects.delete(id)
         return HttpResponse(content=id, headers={"content-type": "application/json"})
+
+    def put(self, request, id):
+        # if Book.objects.filter(id=id).exists():
+        book = Book.objects.filter(id=id)
+        book_data = self.request
+        data_get = b''
+        for b in book_data:
+            data_get += b
+        # print(f"book ---> {data_get}")
+        
+        data_get = json.loads(data_get)
+        print(f"data get ---> {data_get}")
+        # book.update_or_create(id=data_get['id'], title=data_get['title'], photo=data_get['photo'], author=data_get['author'], publish_company=data_get['publish_company'])
+        data_save = Book(id=int(data_get['id']), title=data_get['title'], photo=data_get['photo'])
+        data_save.save()
+        author = Author(name=data_get['author'])
+        author.save()
+        author = Author.objects.get(name=data_get['author'])
+
+        data_save.author.set(author.id)
+        company = PublishCompany(name=data_get['publish_company'])
+        company.save()
+        company = PublishCompany.objects.filter(name=data_get['publis_company']).first()
+        data_save.publish_company.set(company.id)
+
+
+        return HttpResponse(content=data_get, headers={"content-type": "application/json"})
